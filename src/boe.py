49f1f1f8ba4768edffb01fe54004adcfe7a89db7
EndikaMT <44036858+EndikaMomoitio@users.noreply.github.com>
2020-04-13 19:37:31 +0200

    Additions to Boe class and start with class Scrapping
    Added set and get texto to manange later the text of public contracts and start with scrapping where we set the Soap of the html code

--- src/boe.py
+++ src/boe.py
@@ -1,3 +1,9 @@
+from bs4 import BeautifulSoup
+
+import requests
+import datetime
+import csv
+
 class Boe():
 
     def __init__(self, titulo):
@@ -23,3 +29,25 @@ class Boe():
 
     def get_referencia(self):
         return self.referencia
+
+    def set_texto(self, texto):
+        self.texto = texto
+
+    def get_texto(self):
+        return self.texto
+
+class Scraping():
+
+    def get_html(self, url):
+        now = datetime.datetime.now()
+        date = now.strftime("%Y%m%d")
+        # Download HTML
+        #page = requests.get(url + '20200411')
+        page = requests.get(url + date)
+
+        if page.status_code == 200:
+            print("Obtener Páginas del BOE del dia: " + date) 
+            soup = BeautifulSoup(page.content, 'html.parser', from_encoding="utf-8")
+
+            for link in soup.find_all('urlhtm'):
+                self.__analyze_link (link.string)
