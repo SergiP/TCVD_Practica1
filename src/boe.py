1ba627505937d2e5799c631eeab1883fed6484db
Sergio <sergiopostigol@gmail.com>
2020-04-13 18:00:28 +0200

    Create class BOE

---
+++ src/boe.py
@@ -0,0 +1,25 @@
+class Boe():
+
+    def __init__(self, titulo):
+        self.titulo = titulo
+
+    def get_titulo(self):
+        return self.titulo
+
+    def set_seccion(self, seccion):
+        self.seccion = seccion
+
+    def get_seccion(self):
+        return self.seccion
+
+    def set_departamento(self, departamento):
+        self.departamento = departamento
+
+    def get_departamento(self):
+        return self.departamento
+
+    def set_referencia(self, referencia):
+        self.referencia = referencia
+
+    def get_referencia(self):
+        return self.referencia
