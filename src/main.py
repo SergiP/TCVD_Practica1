ab96f1c588475a5c48779f44dca410b76bff0086
EndikaMT <44036858+EndikaMomoitio@users.noreply.github.com>
2020-04-14 12:49:49 +0200

    Update of main.py with functions from BOE class

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from configuration import Configuration
+from boe import Boe, Scraping
 
+# pip install request
+# pip install configparser
+# pip install beautifulsoup4
 
 config = Configuration('config.properties')
+scraping = Scraping()
+scraping.get_html(config.get_url())
